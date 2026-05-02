2b8256fe8e8d69aba4126f0b1afc84ecb73f6203
johsua092-ui <johsua092@gmail.com>
2026-05-03 04:27:52 +0700

    Fix inconsistent math example: Corrected 98x97 logic

---
+++ _next/static/juEvtHBwVY3aZNuPl9fx3/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mtk-simple-//_next/:path+",
+        "destination": "/mtk-simple-/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
